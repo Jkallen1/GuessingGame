7661c1b4d985e9e60ce3c0dc3505631e373df37c
Joshua Allen <jayallen@Joshuas-Air.attlocal.net>
2024-08-05 20:26:57 -0400

    Added comments to each line to help with readability

--- guessingGame.py
+++ guessingGame.py
@@ -1,15 +1,15 @@
-import random
+import random #import random modules to use in func
 
 def guess(x):
-    num = random.randint(1,x)
-    guess = 0
-    while guess != num:
+    num = random.randint(1,x) #storing random number computer selects in num
+    guess = 0 #setting guess to 0 so it will never equal the random num automatically
+    while guess != num: #keeps looping until correct number is guessed
         guess =int(input(f"Enter a number between 1 and {x}: "))
-        if guess < num:
+        if guess < num: #statement if num is to low
             print("Sorry, try again. Your guess is too low.")
-        elif guess > num:
+        elif guess > num: #statement if num is to high
             print("Sorry, try again. Your guess is too high.")
-    print(f"Congratulations you have correctly guessed the number {num}")
+    print(f"Congratulations you have correctly guessed the number {num}") # exits loop and prints this line when correct num guessed
 
 
     guess(10)
